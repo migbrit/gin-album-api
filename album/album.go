9cdcaf39f4f564b95e3248e94c21e0de7995631b
Miguel Brito <miguel.brito@levva.io>
2024-01-17 17:40:27 -0300

    feat: implementing post method using mysql exec

--- album/album.go
+++ album/album.go
@@ -10,16 +10,10 @@ import (
 var db *sql.DB
 
 type album struct {
-	ID     string  `json:"id"`
+	ID     int64   `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func InitializeDb(database *sql.DB) {
@@ -30,10 +24,18 @@ func PostAlbums(ginC *gin.Context) {
 	var newAlbum album
 
 	if err := ginC.BindJSON(&newAlbum); err != nil {
+		ginC.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid JSON data"})
 		return
 	}
 
-	albums = append(albums, newAlbum)
+	result, err := db.Exec("INSERT INTO Albums (Title, Artist, Price) VALUES (?, ?, ?)", newAlbum.Title, newAlbum.Artist, newAlbum.Price)
+	if err != nil {
+		ginC.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Error inserting new album into the database"})
+		return
+	}
+
+	newAlbum.ID, err = result.LastInsertId()
+
 	ginC.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
