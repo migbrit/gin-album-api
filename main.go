23bef8380b0628abf38c2e2a10056e076401b76b
Miguel Brito <miguel.brito@levva.io>
2024-01-17 15:17:06 -0300

    feat: adding support to mysql database

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"gin-simple-api/album"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("An error has occurred trying to get data about .env file -> ", err)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dbConnectionString := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
+
+	db, err := sql.Open("mysql", dbConnectionString)
+	if err != nil {
+		log.Fatal("An error has occurred while trying to connect to the database -> ", err)
+	} else {
+		log.Println("Successfully connected to the database.")
+	}
+
+	defer db.Close()
+
 	router := gin.Default()
 	router.GET("/all-albums", album.GetAlbums)
 	router.GET("/albums", album.GetAlbumByID)
