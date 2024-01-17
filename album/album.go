3a89ddda1c1d5684e51011da439663d5e495aa7f
Miguel Brito <miguel.brito@levva.io>
2024-01-17 17:26:56 -0300

    feat: adding get album by id with mysql support

--- album/album.go
+++ album/album.go
@@ -26,6 +26,49 @@ func InitializeDb(database *sql.DB) {
 	db = database
 }
 
+func PostAlbums(ginC *gin.Context) {
+	var newAlbum album
+
+	if err := ginC.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	ginC.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func GetAlbumByID(ginC *gin.Context) {
+	id := ginC.Query("id")
+
+	if id == "" {
+		ginC.IndentedJSON(http.StatusBadRequest, gin.H{"message:": "Empty id on url"})
+		return
+	}
+
+	println("id: ", id)
+
+	rows, err := db.Query("SELECT Id, Title, Artist, Price from Albums WHERE Id = ?", id)
+	if err != nil {
+		ginC.IndentedJSON(http.StatusNotFound, gin.H{"message:": "Album not found with the specified id."})
+	}
+
+	defer rows.Close()
+
+	if !rows.Next() {
+		ginC.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found with the specified id."})
+		return
+	}
+
+	var a album
+	if err := rows.Scan(&a.ID, &a.Title, &a.Artist, &a.Price); err != nil {
+		ginC.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Error scanning albums on database"})
+		return
+	}
+
+	ginC.IndentedJSON(http.StatusOK, a)
+
+}
+
 func GetAlbums(ginC *gin.Context) {
 	rows, err := db.Query("SELECT Id, Title, Artist, Price from Albums")
 	if err != nil {
@@ -46,32 +89,4 @@ func GetAlbums(ginC *gin.Context) {
 	}
 
 	ginC.IndentedJSON(http.StatusOK, fetchedAlbums)
-}
-
-func PostAlbums(ginC *gin.Context) {
-	var newAlbum album
-
-	if err := ginC.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	albums = append(albums, newAlbum)
-	ginC.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-func GetAlbumByID(ginC *gin.Context) {
-	id := ginC.Query("id")
-
-	if id == "" {
-		ginC.IndentedJSON(http.StatusBadRequest, gin.H{"message:": "empty id on url"})
-		return
-	}
-
-	for _, a := range albums {
-		if a.ID == id {
-			ginC.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	ginC.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
